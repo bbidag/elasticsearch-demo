27d47b5e19adb47d05856e6fbd9ed8481a89b049
HoHoKun <skawlgh112@gmail.com>
2022-11-26 17:08:06 +0900

    first elasticsearch provisioning commit.

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "start elasticsearch server."
+
+docker-compose down
+docker-compose up -d
+
+STATUS=400
+CHECK_CNT=0
+
+while [ $CHECK_CNT -le 10 ]
+do
+  echo "check count for elasticsarch init: ${CHECK_CNT}"
+  ((CHECK_CNT++))
+  sleep 5
+  STATUS=$(curl -o /dev/null -w "%{http_code}" "http://localhost:9200/")
+  if [ $STATUS -eq 200 ]; then
+    echo "elasticsearch start complete."
+    break
+  fi
+done
+
+echo "setting index for demo."
+
+MAPPING=$(cat ./mappings.json)
+
+curl -XPUT "http://127.0.0.1:9200/item" -H 'Content-Type: application/json' -d "${MAPPING}"
+
+echo "indexing demo data."
+
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@index_data.txt"; echo
+echo "Finish provisioning"
